8dff49ed6573f56935ac53486fe58cae941134b3
Aneudy Adames <a4aneudy@gmail.com>
2023-09-28 13:52:25 -0400

    first commit

--- DS4Windows/DS4Control/StickOutCurve.cs
+++ DS4Windows/DS4Control/StickOutCurve.cs
@@ -19,13 +19,19 @@ namespace DS4Windows.StickModifiers
             //Bezier,
         }
 
+        private static SmoothedInput smoothedX = new SmoothedInput(5); // Buffer size 5 for example
+        private static SmoothedInput smoothedY = new SmoothedInput(5);
+
         public static void CalcOutValue(Curve type, double axisXValue, double axisYValue,
             out double axisOutXValue, out double axisOutYValue)
         {
+            double smoothedAxisXValue = smoothedX.AddValue(axisXValue);
+            double smoothedAxisYValue = smoothedY.AddValue(axisYValue);
+
             if (type == Curve.Linear)
             {
-                axisOutXValue = axisXValue;
-                axisOutYValue = axisYValue;
+                axisOutXValue = smoothedAxisXValue;
+                axisOutYValue = smoothedAxisYValue;
                 return;
             }
 
@@ -57,39 +63,48 @@ namespace DS4Windows.StickModifiers
                     {
                         double absX = Math.Abs(tempRatioX);
                         double absY = Math.Abs(tempRatioY);
-                        double temp = outputXValue;
+                        double tempX, tempY;
 
-                        if (absX <= 0.4)
+                        // More gradual and controlled change in the lower range for maximum precision
+                        if (absX <= 0.1) // Extremely gradual in the very low range
                         {
-                            temp = 0.8 * absX;
+                            tempX = 0.25 * absX; // Very low coefficient for maximum precision
+                        }
+                        else if (absX <= 0.5) // Gradual change in the lower range
+                        {
+                            tempX = 0.5 * absX; // Low coefficient for enhanced precision
                         }
                         else if (absX <= 0.75)
                         {
-                            temp = absX - 0.08;
+                            tempX = absX - 0.025; // Slightly adjusted offset for more precision
                         }
-                        else if (absX > 0.75)
+                        else
                         {
-                            temp = (absX * 1.32) - 0.32;
+                            tempX = (absX * 1.2) - 0.2; // Adjusted coefficient and offset for maintaining range
                         }
 
-                        outputXValue = signX * temp * capX;
+                        outputXValue = signX * tempX * capX;
+                        // Similar adjustments for Y axis
 
-                        if (absY <= 0.4)
+                        if (absY <= 0.1) // Extremely gradual in the very low range
                         {
-                            temp = 0.8 * absY;
+                            tempY = 0.25 * absY; // Very low coefficient for maximum precision
+                        }
+                        else if (absY <= 0.5) // Gradual change in the lower range
+                        {
+                            tempY = 0.5 * absY; // Low coefficient for enhanced precision
                         }
                         else if (absY <= 0.75)
                         {
-                            temp = absY - 0.08;
+                            tempY = absY - 0.025; // Slightly adjusted offset for more precision
                         }
-                        else if (absY > 0.75)
+                        else
                         {
-                            temp = (absY * 1.32) - 0.32;
+                            tempY = (absY * 1.2) - 0.2; // Adjusted coefficient and offset for maintaining range
                         }
 
-                        outputYValue = signY * temp * capY;
+                        outputYValue = signY * tempY * capY;
                     }
-
                     break;
 
                 case Curve.Quadratic:
@@ -141,6 +156,31 @@ namespace DS4Windows.StickModifiers
 
             axisOutXValue = outputXValue;
             axisOutYValue = outputYValue;
+        }
+    }
+
+    private class SmoothedInput
+    {
+        private readonly Queue<double> _buffer = new Queue<double>();
+        private readonly int _bufferSize;
+        private double _sum;
+
+        public SmoothedInput(int bufferSize)
+        {
+            _bufferSize = bufferSize;
+        }
+
+        public double AddValue(double value)
+        {
+            if (_buffer.Count >= _bufferSize)
+            {
+                _sum -= _buffer.Dequeue();
+            }
+
+            _buffer.Enqueue(value);
+            _sum += value;
+
+            return _sum / _buffer.Count;
         }
     }
 }
